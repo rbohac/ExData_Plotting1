35f86223f64e0b25cba3a1712d86ed83acae03e2
Ray Bohac <rbohac@gmail.com>
2015-07-10 15:41:57 -0400

    Plot 2

---
+++ plot2.R
@@ -0,0 +1,13 @@
+setwd("C:/source-code/Ray/Coursera/ExData_Plotting1")
+library(lubridate)
+mydata = read.csv(file="household_power_consumption.txt",head=TRUE,stringsAsFactors = FALSE,sep=";")
+mydata$date_and_time <- with(mydata,strptime(paste(mydata$Date," ",mydata$Time), "%d/%m/%Y %H:%M:%S"))
+mydata$dayofweek <- with(mydata,wday(date_and_time,label=T))
+
+subdata<- mydata[dmy(mydata$Date) >= ymd("2007-02-01") & dmy(mydata$Date) <= ymd("2007-02-02"),]
+
+subdata$Global_active_power_kw <- with(subdata,as.numeric(subdata$Global_active_power)/1000)
+subdata$Global_active_power_num <- with(subdata,as.numeric(subdata$Global_active_power))
+plot(subdata$date_and_time, subdata$Global_active_power_num, type = "l",ylab="Global Active Power (kilowatts)",xlab="")
+dev.copy(png,file="plot2.png")
+dev.off()
